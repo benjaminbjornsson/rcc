517ae43200b2fd146039de3e8cef4e54c7a54ac4
Benjamin Björnsson <benjamin.bjornsson@gmail.com>
2025-11-05 22:36:42 +0100

    Move all parse functions to the Parser module
    Let parser handle the parsing instead of the
    individual non-terminal statement constructs.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,7 @@
+use crate::ast::{Exp, Function, Program, Statement};
 use crate::lexer::Lexer;
 use crate::span::Span;
-use crate::token::{Token, TokenKind};
+use crate::token::{Const, Keyword, Token, TokenKind};
 use crate::error::ParseError;
 use crate::ast::Program;
 
@@ -14,11 +15,7 @@ impl<'a> Parser<'a> {
     }
 
     pub fn parse(&mut self) -> Result<Program, ParseError> {
-        let program = Program::parse(self)?;
-
-        self.expect_eof()?;
-
-        Ok(program)
+        self.parse_program()
     }
 
     pub fn expect(&mut self, expected: TokenKind) -> Result<(), ParseError> {
@@ -44,6 +41,64 @@ impl<'a> Parser<'a> {
     }
 }
 
+impl<'a> Parser<'a> {
+    pub fn parse_program(&mut self) -> Result<Program, ParseError> {
+        let function_definition = self.parse_function()?;
+
+        self.expect_eof()?;
+
+        Ok(Program::FunctionDefinition(function_definition))
+    }
+
+    pub fn parse_function(&mut self) -> Result<Function, ParseError> {
+        self.expect(TokenKind::Keyword(Keyword::Int))?;
+
+        let token = self.next()?;
+        let identifier = match token.kind {
+            TokenKind::Identifier(i) => i,
+            _ => {
+                return Err(ParseError::UnexpectedToken(
+                    token,
+                    TokenKind::Identifier(String::from("")),
+                ))
+            }
+        };
+
+        self.expect(TokenKind::OpenParenthesis)?;
+        self.expect(TokenKind::Keyword(Keyword::Void))?;
+        self.expect(TokenKind::CloseParenthesis)?;
+
+        self.expect(TokenKind::OpenBrace)?;
+        let statement = self.parse_statement()?;
+        self.expect(TokenKind::CloseBrace)?;
+
+        Ok(Function::Function {
+            name: identifier,
+            body: statement,
+        })
+    }
+
+    fn parse_statement(&mut self) -> Result<Statement, ParseError> {
+        self.expect(TokenKind::Keyword(Keyword::Return))?;
+        let exp = self.parse_exp()?;
+        self.expect(TokenKind::Semicolon)?;
+
+        Ok(Statement::Return(exp))
+    }
+
+    fn parse_exp(&mut self) -> Result<Exp, ParseError> {
+        let token = self.next()?;
+
+        match token.kind {
+            TokenKind::Constant(Const::Int(num)) => Ok(Exp::ConstantInt(num)),
+            _ => Err(ParseError::UnexpectedToken(
+                token,
+                TokenKind::Constant(Const::Int(0)),
+            )),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +108,119 @@ mod tests {
         let lexer = Lexer::new(";");
         let mut parser = Parser::new(lexer);
         assert!(matches!(parser.expect(TokenKind::Semicolon), Ok(())));
+    }
+
+    mod program {
+        use super::*;
+        use crate::ast::{Exp, Statement};
+        use crate::lexer::Lexer;
+        use crate::token::{Token, TokenKind};
+
+        #[test]
+        fn return_int() {
+            let file = "int main(void) {
+                return 2;
+            }";
+            let lexer = Lexer::new(file);
+            let mut parser = Parser::new(lexer);
+            assert!(matches!(
+                parser.parse_program(),
+                Ok(Program::FunctionDefinition(Function::Function {
+                    name: identifier,
+                    body: Statement::Return(Exp::ConstantInt(2))
+                })) if identifier == "main"
+            ));
+        }
+
+        #[test]
+        fn return_int_missing_semicolon() {
+            let file = "int main(void) {
+                return 2
+            }";
+            let lexer = Lexer::new(file);
+            let mut parser = Parser::new(lexer);
+            assert!(matches!(
+                parser.parse_program(),
+                Err(ParseError::UnexpectedToken(Token { kind, span: _ }, TokenKind::Semicolon ))
+                if kind == TokenKind::CloseBrace
+            ));
+        }
+    }
+
+    mod function {
+        use super::*;
+        use crate::ast::Exp;
+        use crate::lexer::Lexer;
+
+        #[test]
+        fn parse_return() {
+            let file = "int main(void) {
+                return 2;
+            }";
+            let lexer = Lexer::new(file);
+            let mut parser = Parser::new(lexer);
+            assert!(matches!(
+                parser.parse_function(),
+                Ok(Function::Function {
+                    name: identifier,
+                    body: Statement::Return(Exp::ConstantInt(2))
+                }) if identifier == "main"
+            ));
+        }
+    }
+
+    mod statement {
+        use super::*;
+        use crate::lexer::Lexer;
+        use crate::span::Span;
+        use crate::token::{Const, Token, TokenKind};
+
+        #[test]
+        fn parse_return() {
+            let lexer = Lexer::new("return 2;");
+            let mut parser = Parser::new(lexer);
+            assert!(matches!(
+                parser.parse_statement(),
+                Ok(Statement::Return(Exp::ConstantInt(2)))
+            ));
+        }
+
+        #[test]
+        fn parse_missing_semicolon() {
+            let lexer = Lexer::new("return 2 2");
+            let mut parser = Parser::new(lexer);
+            assert!(matches!(
+                parser.parse_statement(),
+                Err(ParseError::UnexpectedToken(
+                    Token {
+                        kind,
+                        span: _
+                    },
+                    TokenKind::Semicolon
+                ))
+                if kind == TokenKind::Constant(Const::Int(2))));
+        }
+
+        #[test]
+        fn parse_missing_semicolon_eof() {
+            let lexer = Lexer::new("return 2");
+            let mut parser = Parser::new(lexer);
+            assert!(matches!(
+                parser.parse_statement(),
+                Err(ParseError::UnexpectedEof(Span { start, end }))
+                if start == 8 && end == 9
+            ));
+        }
+    }
+
+    mod exp {
+        use super::*;
+
+        #[test]
+        fn parse_const_int() {
+            let lexer = Lexer::new("5");
+            let mut parser = Parser::new(lexer);
+            assert!(matches!(parser.parse_exp(), Ok(Exp::ConstantInt(5))));
+        }
     }
 }
